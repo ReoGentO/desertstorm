2b59dd538ea4cdc8bebe66a063206f4c47df6d52
ReoGentO <104772613+ReoGentO@users.noreply.github.com>
2026-08-04 21:58:06 +0500

    Add DownloadAssets.lua to fetch image assets
    This script downloads image assets from specified links and saves them to a designated folder if they do not already exist.

---
+++ DownloadAssets.lua
@@ -0,0 +1,33 @@
+local dir = "Reodesert"
+
+if isfolder and not isfolder(dir) then
+    makefolder(dir)
+end
+
+local images = {
+    eye = {
+        filename = "eye.png",
+        link = "https://github.com/ReoGentO/desertstorm/blob/main/eye.png?raw=true"
+    },
+    cross = {
+        filename = "cross.png",
+        link = "https://github.com/ReoGentO/desertstorm/blob/main/locate-fixed.png?raw=true"
+    }
+}
+
+local ref = {}
+
+for key, info in pairs(images) do
+    local filePath = dir .. "/" .. info.filename
+    
+    if isfile and not isfile(filePath) then
+        local raw_file = game:HttpGet(info.link)
+        writefile(filePath, raw_file)
+    end
+    
+    if getcustomasset then
+        ref[key] = getcustomasset(filePath)
+    end
+end
+
+return ref
